dbb07ac9a8aa83fed29866e2f21458b105f9d432
bohdanchupa <39186990+bohdanchupa@users.noreply.github.com>
2026-09-22 16:53:11 +0300

    Deploy: review P0 — header, motion, disclosure, corrected figures

--- _next/static/chunks/app/[locale]/get-help/page-05abaa9890e6597b.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[34],{9376:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,2619,23)),Promise.resolve().then(n.bind(n,5051)),Promise.resolve().then(n.bind(n,5526))}},e=>{e.O(0,[619,305,60,441,255,358],()=>e(e.s=9376)),_N_E=e.O()}]);
--- _next/static/chunks/app/[locale]/partners/page-a3fe1583e6fec977.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[989],{6241:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,2619,23)),Promise.resolve().then(n.t.bind(n,1356,23)),Promise.resolve().then(n.bind(n,5051)),Promise.resolve().then(n.bind(n,5526))}},e=>{e.O(0,[619,305,356,60,441,255,358],()=>e(e.s=6241)),_N_E=e.O()}]);
